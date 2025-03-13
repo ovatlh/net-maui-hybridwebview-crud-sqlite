4f867899a7e765b4aa0b2da85c9a54fb69402950
Tavo López Hernández <61797363+ovatlh@users.noreply.github.com>
2025-03-13 17:36:54 -0600

    quasar spa example

---
+++ quasar-test-04/src/layouts/MainLayout.vue
@@ -0,0 +1,90 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+
+      <q-page-sticky position="bottom-right" :offset="[18, 18]">
+        <q-fab vertical-actions-align="right" color="primary" glossy icon="keyboard_arrow_up" direction="up">
+          <q-fab-action label-position="left" color="primary" icon="mail" label="Email" />
+          <q-fab-action label-position="left" color="secondary" icon="alarm" label="Alarm" />
+          <q-fab-action label-position="left" color="orange" icon="airplay" label="Airplay" />
+          <q-fab-action label-position="left" color="accent" icon="room" label="Map" />
+        </q-fab>
+      </q-page-sticky>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import EssentialLink, { type EssentialLinkProps } from "components/EssentialLink.vue";
+
+const linksList: EssentialLinkProps[] = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+  {
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
+  },
+  {
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
+  },
+  {
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
+  },
+  {
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
+  },
+  {
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
